20941c96d977bfe50917d006bf666b0ef67a944b
Indra <113356330+Indra8969@users.noreply.github.com>
2023-10-01 14:56:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function move() {
+
+  document.querySelector("#filterop").style.transform =  "translateX(0vw)";
+ document.querySelector("#filterop").style.display =  "block";
+
+}
+
+function movecancel() {
+
+  document.querySelector("#filterop").style.transform =  "translateX(-100vw)";
+document.querySelector("#filterop").style.display =  "none";
+
+}
