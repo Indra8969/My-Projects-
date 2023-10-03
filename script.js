e60cad8ba87cf230df77a3370e3a61cbabb27478
Indra <113356330+Indra8969@users.noreply.github.com>
2023-10-03 09:44:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var btn = documentmquerySelectoAll(".btn")
+
+
+
+function equal() {
+ document.querySelector("input").value = eval( document.querySelector("input").value)
+}
+
+function ac() {
+  
+}
