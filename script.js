ee2170257759c7d5dd6627041338a5961390c0a3
Indra <113356330+Indra8969@users.noreply.github.com>
2023-10-03 14:39:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var btn = documentmquerySelectoAll(".btn")
-
+var buttons = document.querySelectorAll(".btn");
 
 
 function equal() {
@@ -7,5 +6,56 @@ function equal() {
 }
 
 function ac() {
-  
+ document.querySelector("input").value = "";
+}
+function ce() {
+ document.querySelector("input").value = document.querySelector("input").value.slice(0,-1)
+}
+function devide() {
+ document.querySelector("input").value = document.querySelector("input").value + "/";
+}
+function seven() {
+ document.querySelector("input").value = document.querySelector("input").value + "7";
+}
+function eight() {
+ document.querySelector("input").value = document.querySelector("input").value + "8";
+}
+function nine() {
+ document.querySelector("input").value = document.querySelector("input").value + "9";
+}
+function multi() {
+ document.querySelector("input").value = document.querySelector("input").value + "*";
+}
+function four() {
+ document.querySelector("input").value = document.querySelector("input").value + "4";
+}
+function five() {
+ document.querySelector("input").value = document.querySelector("input").value + "5";
+}
+function six() {
+ document.querySelector("input").value = document.querySelector("input").value + "6";
+}
+function minus() {
+ document.querySelector("input").value = document.querySelector("input").value + "-";
+}
+function one() {
+ document.querySelector("input").value = document.querySelector("input").value + "1";
+}
+function two() {
+ document.querySelector("input").value = document.querySelector("input").value + "2";
+}
+function three() {
+ document.querySelector("input").value = document.querySelector("input").value + "3";
+}
+function plus() {
+ document.querySelector("input").value = document.querySelector("input").value + "+";
+}
+function zero() {
+ document.querySelector("input").value = document.querySelector("input").value + "0";
+}
+function dblzero() {
+ document.querySelector("input").value = document.querySelector("input").value + "00";
+}
+function dot() {
+ document.querySelector("input").value = document.querySelector("input").value + ".";
 }
