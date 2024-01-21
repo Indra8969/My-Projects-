5ce6d601434f0930ea06b5080051be403dab037e
Indra <113356330+Indra8969@users.noreply.github.com>
2024-01-21 10:57:07 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -11,57 +11,13 @@ function ac() {
 function ce() {
  document.querySelector("input").value = document.querySelector("input").value.slice(0,-1)
 }
-function devide() {
- document.querySelector("input").value = document.querySelector("input").value + "/";
-}
-function seven() {
- document.querySelector("input").value = document.querySelector("input").value + "7";
-}
-function eight() {
- document.querySelector("input").value = document.querySelector("input").value + "8";
-}
-function nine() {
- document.querySelector("input").value = document.querySelector("input").value + "9";
-}
-function multi() {
- document.querySelector("input").value = document.querySelector("input").value + "*";
-}
-function four() {
- document.querySelector("input").value = document.querySelector("input").value + "4";
-}
-function five() {
- document.querySelector("input").value = document.querySelector("input").value + "5";
-}
-function six() {
- document.querySelector("input").value = document.querySelector("input").value + "6";
-}
-function minus() {
- document.querySelector("input").value = document.querySelector("input").value + "-";
-}
-function one() {
- document.querySelector("input").value = document.querySelector("input").value + "1";
-}
-function two() {
- document.querySelector("input").value = document.querySelector("input").value + "2";
-}
-function three() {
- document.querySelector("input").value = document.querySelector("input").value + "3";
-}
-function plus() {
- document.querySelector("input").value = document.querySelector("input").value + "+";
-}
-function zero() {
- document.querySelector("input").value = document.querySelector("input").value + "0";
-}
-function dblzero() {
- document.querySelector("input").value = document.querySelector("input").value + "00";
-}
-function dot() {
- document.querySelector("input").value = document.querySelector("input").value + ".";
-}
 
-
-
+buttons.forEach((btn)=>{
+ btn.addEventListener("click",()=>{
+  document.querySelector("input").value += btn.innerHTML
+ })
+})
+ 
 
 
 
