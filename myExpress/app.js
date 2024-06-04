9ded2212339b7a41282cb412a2c83146cc31bc75
Indra <113356330+Indra8969@users.noreply.github.com>
2024-06-04 07:13:22 +0530

    Create app.js

---
+++ myExpress/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3001;
+
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/api/projects', (req, res) => {
+  res.json({
+    projects: ["sudoku","typing test","translate","weather","calculator","snake","TicTacToe","pong"]
+  })
+});
+
+
+app.get('/:route', (req, res) => {
+  res.render(req.params.route);
+});
+
+app.listen(port, () => {
+  console.log("listening at port :", port);
+});
